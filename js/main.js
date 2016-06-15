19c89b697b6d421eda4b6ac171f19fff9b7e7104
Wapika <mattldwig@gmail.com>
2016-06-15 11:57:50 +0200

    OpenSearch switch and while loop
    Switch to OpenSearch and to while loop.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-$('.submit').on('click', (noName))
+$('.submit').on('click', (wikiSearch))
 
 function wikiSearch(){
+	$('.responses').empty();
 var searchTerm = document.getElementById('search').value;
 		$.ajax({
-		url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=intitle%3A'+searchTerm,
+		url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&list=search&utf8=1&formatversion=2&suggest=true&search='+searchTerm,
 		dataType: 'jsonp',
 		success: function(data){
-			console.log(data);
+		var answers = data[2];
+		var i = 0;
+			while(answers[i]){
+				$('.responses').append("<div class='responseCard'><a href="+data[3][i]+">"+answers[i]+"</a></div>");
+			i++;
+			}
 		}
 	})
 };
+
 
 $('input').keypress(function(e){
 	if(e.keyCode === 13){
