265e2884022a07311c661644099859c6a2be6757
Wapika <mattldwig@gmail.com>
2016-06-16 17:56:03 +0200

    Refactoring CSS
    Refactoring and improve CSS.

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ $('.submit').on('click', (wikiSearch))
 
 function wikiSearch(){
 	$('.responses').empty();
-var searchTerm = document.getElementById('search').value;
+	var searchTerm = document.getElementById('search').value;
 		$.ajax({
-		url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&list=search&utf8=1&formatversion=2&suggest=true&search='+searchTerm,
+		url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&utf8=1&formatversion=2&suggest=true&search='+searchTerm,
 		dataType: 'jsonp',
 		success: function(data){
-		var answers = data[2];
+		var answers = data[1];
 		var i = 0;
 			while(answers[i]){
-				$('.responses').append("<div class='responseCard'><a href="+data[3][i]+">"+answers[i]+"</a></div>");
-			i++;
+				$('.responses').append("<ul class='responseCard'><li><a href="+data[3][i]+"><h2>"+answers[i]+"</h2></a><p>"+data[2][i]+"</p></li></ul>");
+				i++;
 			}
 		}
 	})
