d331147dc2c445d885b75cbbbce94bff5758b52c
Wapika <mattldwig@gmail.com>
2016-06-19 15:12:12 +0200

    Refactoring & fix Firefox
    Fix Firefox bug (Flexbox & position: absolute);
    Code refactoring.
    Finalisation;

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
-$('.submit').on('click', (wikiSearch))
-
 function wikiSearch(){
-	$('.responses').empty();
+  $('.responses').empty();
 	var searchTerm = document.getElementById('search').value;
 		$.ajax({
-		url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&utf8=1&formatversion=2&suggest=true&search='+searchTerm,
-		dataType: 'jsonp',
-		success: function(data){
-		if(data[1].length !== 0){
-		$('.searchBox').addClass('header');
-		var answers = data[1];
-		var i = 0;
-			while(answers[i]){
-				$('.responses').append("<ul class='responseCard'><li><a href="+data[3][i]+"><h2>"+answers[i]+"</h2></a><p>"+data[2][i]+"</p></li></ul>");
-				i++;
-			}
-			}
-		else{
-			$('.responses').append("<p>The page "+searchTerm+" does not exist. You can <a href='https://en.wikipedia.org/wiki/Wikipedia:Articles_for_creation'>ask for it to be created</a>.");
-		}
-		}
+		  url: 'https://en.wikipedia.org/w/api.php?   action=opensearch&format=json&formatversion=2&utf8=1&formatversion=2&callback=?&search='+searchTerm,
+		  dataType: 'jsonp',
+		  success: function(data){
+		    if(data[1].length !== 0){
+		      $('.searchBox').addClass('header');
+		      var answers = data[1];
+		      var i = 0;
+			      while(answers[i]){
+				    $('.responses').append("<ul class='responseCard'><li><a href="+data[3][i]+"><h2>"+answers[i]+"</h2></a><p>"+data[2][i]+"</p></li></ul>");
+				    i++;
+			      }
+			   } else{
+			      $('.responses').append("<p>The page "+searchTerm+" does not exist. You can <a href='https://en.wikipedia.org/wiki/Wikipedia:Articles_for_creation'>ask for it to be created</a>.");
+		    }
+		  }
 	})
 };
 
+//Call wikiSearch if Enter is pressed
 $('input').keypress(function(e){
 	if(e.keyCode === 13){
 	wikiSearch();			
